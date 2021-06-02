96ffe6bb8e3f0dda7ad05a7f98ef9e2fe0fbf240
ppotatoG <thgus2270@gmail.com>
2021-06-02 11:25:07 +0900

    탭 제이쿼리 추가

--- example01/js/js.js
+++ example01/js/js.js
@@ -1,13 +1,25 @@
 $(document).ready(function(){
-    setInterval(function(){
-        $('.slide ul').animate({
-            top: '-300px'
-        },function(){
-            $('.slide ul li').eq(0).appendTo('.slide ul');
-            $('.slide ul').css({
-                top:0
-            });
-        });
+    // setInterval(function(){
+    //     $('.slide ul').animate({
+    //         top: '-300px'
+    //     },function(){
+    //         $('.slide ul li').eq(0).appendTo('.slide ul');
+    //         $('.slide ul').css({
+    //             top:0
+    //         });
+    //     });
         
-    },3000);
+    // },3000);
+
+    
+});
+$('.board .list a').on('click', function(){
+    $(this).parent().parent().children().removeClass('on')
+    $(this).parent().addClass('on')
+
+
+    var contType = $(this).data('type')
+
+    $('.'+contType).parent().children().removeClass('on')
+    $('.'+contType).addClass('on')
 });
