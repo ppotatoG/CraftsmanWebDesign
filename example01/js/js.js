4744740d709ca68be8637be89ad70926b8613f57
ppotatoG <thgus2270@gmail.com>
2021-06-02 11:40:56 +0900

    z-index 추가

--- example01/js/js.js
+++ example01/js/js.js
@@ -1,17 +1,15 @@
 $(document).ready(function(){
-    // setInterval(function(){
-    //     $('.slide ul').animate({
-    //         top: '-300px'
-    //     },function(){
-    //         $('.slide ul li').eq(0).appendTo('.slide ul');
-    //         $('.slide ul').css({
-    //             top:0
-    //         });
-    //     });
+    setInterval(function(){
+        $('.slide ul').animate({
+            top: '-300px'
+        },function(){
+            $('.slide ul li').eq(0).appendTo('.slide ul');
+            $('.slide ul').css({
+                top:0
+            });
+        });
         
-    // },3000);
-
-    
+    },3000);    
 });
 $('.board .list a').on('click', function(){
     $(this).parent().parent().children().removeClass('on')
