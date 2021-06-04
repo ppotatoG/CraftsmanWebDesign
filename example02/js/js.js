04a1e0420d087fccb0bb22b4324f1a272cce6b3f
ppotatoG <thgus2270@gmail.com>
2021-06-04 16:05:04 +0900

    header 완료

--- example02/js/js.js
+++ example02/js/js.js
@@ -1,11 +1,7 @@
 $(document).ready(function(){
-    setInterval(function(){
-        $('.slide ul').animate({
-                left : '-1200px'
-            },function(){
-                $('.slide ul li').eq(0).appendTo('.slide ul')
-                $('.slide ul').css({left : 0})
-            })
-    }, 3000)
-    
+    $('.nav, .depth_bg').hover(function(){
+        $('.depth, .depth_bg').stop().slideDown()	
+    }, function(){
+        $('.depth, .depth_bg').stop().slideUp()	
+    })
 })
