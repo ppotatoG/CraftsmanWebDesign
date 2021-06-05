db4d9cd0f9fc8cc422c5a42ef8228bca9a70d9f8
ppotatoG <thgus2270@gmail.com>
2021-06-05 21:31:22 +0900

    완성 - 40분

---
+++ example03/js/js.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $('.slide ul li').eq(num).fadeIn()
+
+    setInterval(function(){
+        let num = 0
+
+        if(num < 2) {
+            num ++
+        } else {
+            num = 0
+        }
+
+        $('.slide ul li').fadeOut()
+        $('.slide ul li').eq(num).fadeIn()
+
+    }, 3000)
+
+    $('.tab li').on('click', function(){
+
+        $('.tab li').removeClass('on')
+        $('.tab .inner >div').removeClass('on')
+
+        let thisType = $(this).data('cont')
+
+        $(this).addClass('on')
+        $('.'+thisType).addClass('on')
+    })
+})
+
+function openPop(){
+    $('.popup').fadeIn()
+}
+
+function closePop(){
+    $('.popup').fadeOut()
+}
