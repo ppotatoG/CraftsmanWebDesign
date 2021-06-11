7e83f6da2ce5ea899c427e6b24047d6de66e6a02
ppotatoG <thgus2270@gmail.com>
2021-06-11 22:18:56 +0900

    완료 - 35분

---
+++ example11/js/js.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    let slideLength = $('.slide ul li').length
+    let slideWidth = $('.slide').width()
+
+    $('.slide ul').width(slideLength*slideWidth+'px')
+
+    console.log($('.slide ul').width())
+
+    setInterval(() => {
+        $('.slide ul').animate({
+            left : -slideWidth
+        }, 
+        
+        slideWidth, 
+        
+        function(){
+            $('.slide ul li').eq(0).appendTo('.slide ul')
+            $('.slide ul').css('left', 0)
+        })
+    }, 3000);
+})
+
+function openPop(el){
+    $(el).fadeIn()
+}
+
+function closePop(el){
+    $(el).fadeOut()
+}
